0852affea8c358c9ae90b8b45b2a2024cd4431b2
Brigitte Huneke <brigittehuneke@gmail.com>
2017-02-16 09:29:56 -0800

    basic server code

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('./lib/app');
+const http = require('http');
+
+require('./lib/connection');
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+    console.log('Server is running on:' + server.address());
+});
