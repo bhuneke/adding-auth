abb2846927978a4fda92a263e2a6e4545150bf8d
Brigitte Huneke <brigittehuneke@gmail.com>
2017-02-28 16:42:36 -0800

    added test for error handler

---
+++ test/error-handler.test.js
@@ -0,0 +1,21 @@
+const errorHandler = require('../lib/error-handler')();
+const assert = require('chai').assert;
+
+describe('error handler', () => {
+
+    it('returns 500 when no code is provided', () => {
+        const err = new Error('server error');
+        const res = {
+            status(code) {
+                this.code = code;
+                return this;
+            },
+            send(error) {
+                this.error = error;
+            }
+        };
+        errorHandler(err, null, res, null);
+        assert.equal(res.code, 500);
+        assert.deepEqual(res.error.error, 'internal server error')
+    });
+});
